02062b2a88aede090295a16363ebd688c1153c49
王丹 <1653591454@qq.com>
2019-03-08 08:46:26 +0800

    基本项目创建

---
+++ IOS-JS/IOS-JS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS-JS
+//
+//  Created by sh-lx on 2019/3/8.
+//  Copyright © 2019年 WangDan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
